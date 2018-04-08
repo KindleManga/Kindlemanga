38ff1c7bea8a90e479865e0c0c2056668170b522
doanhtu <tu0703@gmail.com>
2018-04-08 21:46:17 +0700

    Finish create and uploading manga

--- manga_web/manga/tasks.py
+++ manga_web/manga/tasks.py
@@ -4,13 +4,21 @@ import logging
 import shlex
 import shutil
 import subprocess
+import time
 
-import requests
+from django.conf import settings
 from celery import group, chord, chain
 from celery.decorators import task
+from PIL import Image
+import requests
+from mediafire.client import MediaFireClient, File, Folder
 
 from .utils import url2filename, extract_images_url
 from .models import Manga, Volume, Chapter
+
+client = MediaFireClient()
+client.login(email=settings.MEDIAFIRE_EMAIL, password=settings.MEDIAFIRE_PASSWORD, app_id='42511')
+UPLOAD_FOLDER = settings.MEDIAFIRE_FOLDER
 
 # https://stackoverflow.com/questions/31784484/how-to-parallelized-file-downloads
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
@@ -57,12 +65,41 @@ def download_volume(volume_id):
 
 @task(name="generate_manga")
 def generate_manga(path, profile='KV'):
+    time.sleep(45)
     args = shlex.split('kcc-c2e -m -q -p {0} -f MOBI {1}'.format(profile, shlex.quote(path)))
     p = subprocess.Popen(args, stdout=subprocess.PIPE)
     p.communicate()
     return "{}.mobi".format(path)
 
 
+@task(name="delete_corrupt_file")
+def delete_corrupt_file(path):
+    for filename in os.listdir(path):
+        try:
+            img = Image.open(os.path.join(path, filename))
+            img.verify()
+        except (IOError, SyntaxError) as e:
+            os.remove(os.path.join(path, filename))
+
+    return path
+
+
+@task(name="upload_and_save")
+def upload_and_save(path, volume_id):
+    v = Volume.objects.get(id=volume_id)
+    r = client.upload_file(path, UPLOAD_FOLDER)
+    link = "http://www.mediafire.com/file/{}".format(r.quickkey)
+    v.download_link = link
+    v.save()
+    shutil.rmtree(path.split('.mobi')[0])
+    os.remove(path)
+
+
 def make_volume(volume_id):
-    res = chain(download_volume.s(volume_id), generate_manga.s())()
+    res = chain(
+        download_volume.s(volume_id),
+        delete_corrupt_file.s(),
+        generate_manga.s(),
+        upload_and_save.s(volume_id)
+    )()
     return res
