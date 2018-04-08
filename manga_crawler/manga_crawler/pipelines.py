15a27b49121710e5f34194469269594766cb05e9
doanhtu <tu0703@gmail.com>
2018-04-08 12:01:08 +0700

    Add manga crawler for later

---
+++ manga_crawler/manga_crawler/pipelines.py
@@ -0,0 +1,32 @@
+import logging
+
+from .items import MangaCrawlerItem
+from manga.models import Manga, Volume, Chapter
+
+logger = logging.getLogger(__name__)
+
+
+class MangaCrawlerPipeline(object):
+    def process_item(self, item, spider):
+        manga = Manga(
+            name=item['name'][0],
+            source=item['source'][0],
+            total_chap=item['total_chap'][0],
+            image_src=item['image_src'][0]
+        )
+        manga.save()
+        logger.info('Manga added')
+
+        chapters = item['chapters']
+        k = enumerate(
+            [chapters[x:x+12] for x in range(0, len(chapters), 12)],
+            start=1
+        )
+        for i, chaps in k:
+            vol = Volume(manga=manga, number=i)
+            vol.save()
+            Chapter.objects.bulk_create(
+                [Chapter(volume=vol, name=c[0], source=c[1]) for c in chaps]
+            )
+            logger.info('Volume added')
+        return item
---
+++ manga_crawler/manga_crawler/spiders/hvtt.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+import re
+import scrapy
+
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import TakeFirst, Join, MapCompose
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+from manga_crawler.items import MangaCrawlerItem
+
+
+class HvttSpider(CrawlSpider):
+    name = 'hvtt'
+    allowed_domains = ['hocvientruyentranh.com']
+    start_urls = ['http://hocvientruyentranh.com/manga/all?filter_type=view']
+
+    rules = (
+        Rule(LinkExtractor(restrict_xpaths='//*[@rel="next"]')),
+        Rule(
+            LinkExtractor(restrict_xpaths='//*[@class="ajax-call"]/..'),
+            callback='parse_item',
+        ),
+    )
+
+    def parse_item(self, response):
+        """
+        @url http://hocvientruyentranh.com/manga/2/shokugeki-no-souma-
+        @returns items 1
+        @scrapes name source total_chap chapters
+        """
+        manga = ItemLoader(item=MangaCrawlerItem(), response=response)
+
+        manga.add_xpath('name', '//h3[@class="__name"]/text()', MapCompose(str.strip))
+        manga.add_value('source', response.url)
+        manga.add_xpath('image_src', '//*[@class="__image"]/img/@src')
+        manga.add_xpath(
+            'total_chap',
+            '//*[@class="table table-hover"]/tbody/tr[1]/td[1]/a//text()',
+            TakeFirst(),
+            MapCompose(lambda x: re.findall(r'\d+', x))
+        )
+
+        chapter_source = manga.get_xpath('//*[@class="table table-hover"]/tbody//tr//td//a/@href')
+        chapter_name = manga.get_xpath('//*[@class="table table-hover"]/tbody//tr//td//a//text()')
+        chapters = zip(chapter_name, chapter_source)
+
+        manga.add_value('chapters', chapters)
+
+        return manga.load_item()
