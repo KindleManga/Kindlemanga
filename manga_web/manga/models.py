e9c5876b6f7ab9e8e776fe397452b222fe9b2d4b
Do Anh Tu <tu0703@gmail.com>
2018-12-27 18:44:13 +0700

    Update

--- manga_web/manga/models.py
+++ manga_web/manga/models.py
@@ -6,20 +6,38 @@ from unidecode import unidecode
 
 
 class Manga(models.Model):
+    BLOG_TRUYEN = 'blogtruyen'
+    NET_TRUYEN = 'nettruyen'
+    MANGA_SEE_ONLINE = 'mangaseeonline'
+
+    SOURCE_CHOICES = (
+        (BLOG_TRUYEN, 'blogtruyen'),
+        (NET_TRUYEN, 'nettruyen'),
+        (MANGA_SEE_ONLINE, 'mangaseeonline')
+    )
+
     name = models.CharField(max_length=500, null=False)
+    web_source = models.CharField(max_length=100, choices=SOURCE_CHOICES)
     source = models.URLField(max_length=500, null=False)
     description = models.TextField(null=True)
     total_chap = models.IntegerField(null=True)
     image_src = models.URLField(max_length=500, null=True)
     slug = models.SlugField()
+    full = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.name)
+
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(unidecode(self.name))
+            slug = slugify('{} {}'.format(unidecode(self.name), self.web_source))
+            if Manga.objects.filter(slug=slug).exists():
+                import random
+                import string
+                slug = slug + " " + ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+            self.slug = slug
         super(Manga, self).save(*args, **kwargs)
 
     def as_dict(self):
