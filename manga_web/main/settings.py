0a65215083191d8e14a0d6e3e40b4c3b3391c3d0
tudoanh <tu0703@gmail.com>
2022-06-08 18:32:15 +0700

    fix referrer

--- manga_web/main/settings.py
+++ manga_web/main/settings.py
@@ -201,3 +201,5 @@ CACHEOPS_ENABLED = not DEBUG
 SPLASH_URL = "http://splash:8050/render.html"
 
 USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36"
+
+SECURE_REFERRER_POLICY = "no-referrer-when-downgrade"
