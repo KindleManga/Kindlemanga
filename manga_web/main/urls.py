f5b7fdf19524e817328a999453b40e78740c6c94
tudoanh <tu0703@gmail.com>
2022-06-10 20:43:08 +0700

    add recaptcha for contact

--- manga_web/main/urls.py
+++ manga_web/main/urls.py
@@ -17,11 +17,25 @@ from django.contrib import admin
 from django.conf import settings
 from django.http import HttpResponse
 from django.urls import include, path
+from django.views.generic import TemplateView
+from django_contact_form.views import ContactFormView
+from manga.forms import CustomContactForm
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("manga.urls", namespace="manga")),
-    path("contact/", include("django_contact_form.urls")),
+    path('contact/',
+         ContactFormView.as_view(
+             form_class=CustomContactForm
+         ),
+         name='django_contact_form'),
+    path(
+        "sent/",
+        TemplateView.as_view(
+            template_name="django_contact_form/contact_form_sent.html"
+        ),
+        name="django_contact_form_sent",
+    ),
     path(
         "robots.txt",
         lambda x: HttpResponse(
--- manga_web/manga/forms.py
+++ manga_web/manga/forms.py
@@ -1,6 +1,7 @@
 from captcha.fields import ReCaptchaField
 from captcha.widgets import ReCaptchaV3
 from django import forms
+from django_contact_form.forms import ContactForm
 
 from .models import Manga, Volume
 from .tasks import make_volume
@@ -16,3 +17,7 @@ class CreateVolumeForm(forms.Form):
 
 class SearchForm(forms.Form):
     name = forms.CharField()
+
+
+class CustomContactForm(ContactForm):
+    captcha = ReCaptchaField(widget=ReCaptchaV3)
