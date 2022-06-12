b31dfe6549c254b7731feeb6cfd951f6403da4f1
tudoanh <tu0703@gmail.com>
2022-06-12 23:58:57 +0700

    do not obey robots.txt

--- manga_crawler/manga_crawler/settings.py
+++ manga_crawler/manga_crawler/settings.py
@@ -30,7 +30,7 @@ ITEM_PIPELINES = {
 
 USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36"
 
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 #  Enable and configure the AutoThrottle extension (disabled by default)
 #  See http://doc.scrapy.org/en/latest/topics/autothrottle.html
